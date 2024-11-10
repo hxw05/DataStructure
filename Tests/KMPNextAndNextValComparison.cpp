dab3692bf0f6444bac10e58f3a582085102c6c6f
hxw05 <hxw05@whut.edu.cn>
2024-11-10 23:29:35 +0800

    生成字符串的next和nextval数组test

---
+++ Tests/KMPNextAndNextValComparison.cpp
@@ -0,0 +1,31 @@
+/**
+ * 生成一个字符串的next数组和nextval数组
+ * 用于比较
+ */
+#include "../String.cpp"
+#include "iostream"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++11-extensions"
+using namespace std;
+
+int main() {
+    auto* str = new FixedLengthString;
+    // 函数处理时的下标都从1开始，第一个字符设为0
+    strcpy(str->characters, "0AAABCAABCA");
+    str->length = 10;
+
+    vector<int> next, nextval;
+    nextval.resize(999);
+    next.resize(999);
+
+    GetNextVector(*str, next);
+    GetNextValVector(*str, nextval);
+
+    for (int i = 1; i <= str->length; i++) cout << str->characters[i] << " ";
+    cout << endl;
+    for (int i = 1; i <= str->length; i++) cout << next[i] << " ";
+    cout << endl;
+    for (int i = 1; i <= str->length; i++) cout << nextval[i] << " ";
+}
+#pragma clang diagnostic pop
